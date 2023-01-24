cdc9e4770b2cce98466bf67f2397d7353a5d311e
Anseri1 <99191975+Anseri1@users.noreply.github.com>
2023-01-24 13:36:29 -0500

    Delete components directory

--- components/helperfunctions.lua
+++
@@ -1,8 +0,0 @@
-function getInstForPlayerName(playerName)
-	for index, inst in ipairs(AllPlayers) do
-        if inst:GetDisplayName() == playerName then
-			return inst
-		end
-	end
-	return nil
-end
